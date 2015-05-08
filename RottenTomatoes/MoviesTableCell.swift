70892c89d85825c6e8be65c05af1103d0cd98e05
Baris Taze <baris@uber.com>
2015-05-08 16:56:36 -0700

    Adding movie table cell and associated class

---
+++ RottenTomatoes/MoviesTableCell.swift
@@ -0,0 +1,28 @@
+//
+//  MoviesTableCell.swift
+//  RottenTomatoes
+//
+//  Created by Baris Taze on 5/8/15.
+//  Copyright (c) 2015 Baris Taze. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableCell: UITableViewCell {
+
+    @IBOutlet weak var movieImageView: UIView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieSnopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- RottenTomatoes/MoviesViewController.swift
+++ RottenTomatoes/MoviesViewController.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var movieTableView: UITableView!
+    @IBOutlet weak var movieGridView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.movieTableView.dataSource = self
+        
+        self.movieGridView.removeFromSuperview()
+        self.view.addSubview(self.movieTableView)
+        
+        //self.movieTableView.removeFromSuperview()
+        //self.view.addSubview(self.movieGridView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = self.movieTableView.dequeueReusableCellWithIdentifier("com.baris.rotten.movies.cell", forIndexPath: indexPath) as! MoviesTableCell
+        return cell
     }
     
 

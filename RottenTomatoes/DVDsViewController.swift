ae90cace2d243126233e92064f5ce1dcbe981c24
Baris Taze <baris@uber.com>
2015-05-08 22:54:04 -0700

    loading DVDs

--- RottenTomatoes/DVDsViewController.swift
+++ RottenTomatoes/DVDsViewController.swift
@@ -11,11 +11,28 @@ import UIKit
 class DVDsViewController: UIViewController, UICollectionViewDataSource {
 
     @IBOutlet weak var dvdsCollectionView: UICollectionView!
+
+    var infiniteLoadingStarted:Bool=false
+    var refreshControl:UIRefreshControl!
+    
+    var dvdsData: NSMutableArray!
+    var dvdsDataFiltered: NSMutableArray!
+    
+    typealias onDVDsRetrieved = (NSArray) -> (Void)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.dvdsCollectionView.dataSource = self
+        
+        // create refreshing control
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        self.dvdsCollectionView.insertSubview(self.refreshControl, atIndex: 0)
+        
+        self.dvdsData = NSMutableArray()
+        self.dvdsDataFiltered = NSMutableArray()
+        self.loadMoreDVDsWithOptions(false, removeFooter: false);
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,19 +42,112 @@ class DVDsViewController: UIViewController, UICollectionViewDataSource {
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 10
+        return dvdsDataFiltered?.count ?? 0
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = self.dvdsCollectionView.dequeueReusableCellWithReuseIdentifier("com.baris.rotten.dvds.cell", forIndexPath: indexPath) as! DvdCollectionCell
         
-        let url = NSURL(string:"http://lorempixel.com/400/400/")
-        cell.dvdImageView.setImageWithURL(url)
+        
+        let movie = self.dvdsDataFiltered[indexPath.row] as! NSDictionary;
+        let posters = movie["posters"] as! NSDictionary
+        let posterThumbnail = posters["thumbnail"] as! String
+        let posterThumbnailUrl = NSURL(string: posterThumbnail)
+        cell.dvdImageView.setImageWithURL(posterThumbnailUrl)
+        
+        var count = self.dvdsDataFiltered?.count ?? 0;
+        if(!self.isFiltered() && !self.infiniteLoadingStarted && indexPath.row == (self.dvdsDataFiltered.count-1)){
+            self.loadMoreDVDsWithOptions(false, removeFooter: true);
+        }
         
         return cell
     }
-
+    
+    func loadMoreDVDsWithOptions(endRefreshing:Bool, removeFooter:Bool){
+        
+        self.queryDVDsWithCallback({(data:NSArray)->(Void) in
+            
+            if(endRefreshing){
+                var all = NSMutableArray(array: data as [AnyObject])
+                all.addObjectsFromArray(self.dvdsData as [AnyObject])
+                self.dvdsData = all
+            }
+            else {
+                self.dvdsData.addObjectsFromArray(data as [AnyObject])
+            }
+            
+            self.reloadTableData()
+            
+            if(endRefreshing){
+                self.refreshControl.endRefreshing()
+            }
+            
+            if(removeFooter){
+                self.infiniteLoadingStarted = false;
+                // self.footer.removeFromSuperview()
+            }
+        })
+    }
+    
+    func onRefresh() {
+        
+        if(self.isFiltered()){
+            self.refreshControl.endRefreshing()
+            return
+        }
+        
+        self.loadMoreDVDsWithOptions(true, removeFooter: false);
+    }
+    
+    func queryDVDsWithCallback(callback:onDVDsRetrieved){
+        
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json")!
+        var request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            if (error == nil) {
+                var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary
+                var data = responseDictionary["movies"] as! NSArray
+                callback(data)
+            }
+            else {
+            }
+        }
+    }
+    
+    func reloadTableData(){
+        
+        self.dvdsDataFiltered.removeAllObjects()
+        
+        var filter = getFilter()
+        let length = count(filter)
+        if(length > 0){
+            for movie in self.dvdsData {
+                let title = movie["title"] as? String
+                let synopsis = movie["synopsis"] as? String
+                if((title != nil && title!.rangeOfString(filter) != nil) || (synopsis != nil && synopsis!.rangeOfString(filter) != nil)) {
+                    self.dvdsDataFiltered.addObject(movie as AnyObject)
+                }
+            }
+        }
+        else{
+            self.dvdsDataFiltered.addObjectsFromArray(self.dvdsData as [AnyObject])
+        }
+        
+        self.dvdsCollectionView.reloadData()
+    }
+    
+    func getFilter() -> String {
+        //var filter = self.searchBar.text;
+        //return filter.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        return ""
+    }
+    
+    func isFiltered() -> Bool {
+        return count(self.getFilter()) >  0
+    }
+    
     /*
     // MARK: - Navigation
 

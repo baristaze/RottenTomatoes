e25544a7503435b4515d1166b1b99a15ec484e8a
Baris Taze <baris@uber.com>
2015-05-10 18:12:43 -0700

    Adding height-adjusted synopsis to the DVD details

---
+++ RottenTomatoes/CustomUILabel.swift
@@ -0,0 +1,26 @@
+//
+//  CustomUILabel.swift
+//  RottenTomatoes
+//
+//  Created by Baris Taze on 5/10/15.
+//  Copyright (c) 2015 Baris Taze. All rights reserved.
+//
+
+import UIKit
+
+class CustomUILabel: UILabel {
+
+    override func drawTextInRect(rect: CGRect) {
+        let insets = UIEdgeInsets(top:10, left:10, bottom:10, right:10)
+        return super.drawTextInRect(UIEdgeInsetsInsetRect(rect, insets))
+    }
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- RottenTomatoes/DvdDetailViewController.swift
+++ RottenTomatoes/DvdDetailViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class DvdDetailViewController: UIViewController {
 
     @IBOutlet weak var dvdImageView: UIImageView!
+    @IBOutlet weak var synopsisButtonLabel: UIButton!
     
     var dvdInfo: NSDictionary!
     
@@ -27,8 +28,44 @@ class DvdDetailViewController: UIViewController {
         posterUrl = "http://content6.flixster.com\(posterUrl)"
         let url = NSURL(string: posterUrl)
         self.dvdImageView.setImageWithURL(url)
+
+        let topBorderLine = UIView(frame: CGRect(x:0, y:2, width:self.synopsisButtonLabel.frame.width, height:1))
+        topBorderLine.backgroundColor = UIColor(red:0.8, green:0.8, blue:0.8, alpha:0.8)
+        self.synopsisButtonLabel.addSubview(topBorderLine)
+        
+        self.synopsisButtonLabel.titleLabel?.lineBreakMode = .ByWordWrapping
+        self.synopsisButtonLabel.titleLabel?.textAlignment = .Justified
+        let synopsis = self.dvdInfo["synopsis"] as? String
+        self.synopsisButtonLabel.setTitle(synopsis, forState: UIControlState.Normal)
+        
+        let expectedLabelHeight = 20.0 + self.heightForLabel(
+            synopsis!,
+            font: self.synopsisButtonLabel.titleLabel!.font,
+            width: (self.view.frame.width - 20.0))
+        
+        var currentY:CGFloat = self.synopsisButtonLabel.frame.origin.y
+        var currentVisibleHeight:CGFloat = self.view.frame.height - self.synopsisButtonLabel.frame.origin.y
+        currentVisibleHeight = currentVisibleHeight - self.tabBarController!.tabBar.frame.size.height
+        println(currentVisibleHeight)
+        if(expectedLabelHeight < currentVisibleHeight) {
+            // push down
+            currentY = currentY + (currentVisibleHeight - expectedLabelHeight)
+        }
+        self.synopsisButtonLabel.frame = CGRect(x:0, y:currentY, width:self.view.frame.width, height:expectedLabelHeight)
     }
 
+    func heightForLabel(text:String, font:UIFont, width:CGFloat) -> CGFloat {
+        
+        let label:UILabel = UILabel(frame: CGRectMake(0, 0, width, CGFloat.max))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        label.font = font
+        label.text = text
+        
+        label.sizeToFit()
+        return label.frame.height
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
